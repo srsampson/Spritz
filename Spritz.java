b2613c23a700eec88ed760ac91d2cfc2e5debc14
srsampson <44934883+srsampson@users.noreply.github.com>
2022-07-14 15:29:45 -0500

    Update Spritz.java

--- Spritz.java
+++ Spritz.java
@@ -8,8 +8,6 @@
  * Jacob C. N. Schuldt
  *
  * Translated to Java, Steve Sampson, November 2014
- *
- * 24 bit nonce version
  */
 package spritz;
 
@@ -34,19 +32,19 @@ public final class Spritz {
     private byte[] S_BOX_P;
 
     public Spritz() {
-        this.S_BOX = new byte[N];
+        S_BOX = new byte[N];
     }
 
     public void initialize() {
-        this.i_register = 0;
-        this.j_register = 0;
-        this.k_register = 0;
-        this.z_register = 0;
-        this.a_register = 0;
-        this.w_register = 1;
+        i_register = 0;
+        j_register = 0;
+        k_register = 0;
+        z_register = 0;
+        a_register = 0;
+        w_register = 1;
 
         for (int loop = 0; loop < N; loop++) {
-            this.S_BOX[loop] = (byte) loop;
+            S_BOX[loop] = (byte) loop;
         }
     }
 
@@ -61,7 +59,7 @@ public final class Spritz {
         z_register_p = z_register;
         a_register_p = a_register;
         w_register_p = w_register;
-        this.S_BOX_P = this.S_BOX.clone();
+        S_BOX_P = S_BOX.clone();
     }
 
     /*
@@ -75,7 +73,7 @@ public final class Spritz {
         z_register = z_register_p;
         a_register = a_register_p;
         w_register = w_register_p;
-        this.S_BOX = this.S_BOX_P.clone();
+        S_BOX = S_BOX_P.clone();
     }
 
     private int low(int val1) {
@@ -87,31 +85,31 @@ public final class Spritz {
     }
 
     private void swap(int val1, int val2) {
-        byte temp = this.S_BOX[val1];
-        this.S_BOX[val1] = this.S_BOX[val2];
-        this.S_BOX[val2] = temp;
+        byte temp = S_BOX[val1];
+        S_BOX[val1] = S_BOX[val2];
+        S_BOX[val2] = temp;
     }
 
     private void update() {
         // simulate unsigned
 
-        int temp1 = (this.i_register + this.w_register) & 0xFF;
-        this.i_register = (short) temp1;
+        int temp1 = (i_register + w_register) & 0xFF;
+        i_register = (short) temp1;
 
-        int temp2 = this.S_BOX[this.i_register];
-        temp2 = (this.j_register + (temp2 < 0 ? 256 + temp2 : temp2)) & 0xFF;
+        int temp2 = S_BOX[i_register];
+        temp2 = (j_register + (temp2 < 0 ? 256 + temp2 : temp2)) & 0xFF;
 
-        int temp3 = this.S_BOX[temp2];
-        temp3 = this.k_register + (temp3 < 0 ? 256 + temp3 : temp3);
-        this.j_register = (short) (temp3 & 0xFF);
+        int temp3 = S_BOX[temp2];
+        temp3 = k_register + (temp3 < 0 ? 256 + temp3 : temp3);
+        j_register = (short) (temp3 & 0xFF);
 
-        int temp4 = this.S_BOX[this.j_register];
+        int temp4 = S_BOX[j_register];
         temp4 = temp4 < 0 ? 256 + temp4 : temp4;
 
-        int temp5 = (this.i_register + this.k_register) & 0xFF;
-        this.k_register = (short) ((temp5 + temp4) & 0xFF);
+        int temp5 = (i_register + k_register) & 0xFF;
+        k_register = (short) ((temp5 + temp4) & 0xFF);
 
-        swap(this.i_register, this.j_register);
+        swap(i_register, j_register);
     }
 
     private void whip() {
@@ -119,15 +117,15 @@ public final class Spritz {
             update();
         }
 
-        this.w_register = (short) ((this.w_register + 2) & 0xFF);
+        w_register = (short) ((w_register + 2) & 0xFF);
     }
 
     private void crush() {
         for (int loop = 0; loop < N / 2; loop++) {
             int temp = ((N - 1) - loop) & 0xFF;
 
-            int c1 = (this.S_BOX[loop] < 0 ? 256 + this.S_BOX[loop] : this.S_BOX[loop]);
-            int c2 = (this.S_BOX[temp] < 0 ? 256 + this.S_BOX[temp] : this.S_BOX[temp]);
+            int c1 = (S_BOX[loop] < 0 ? 256 + S_BOX[loop] : S_BOX[loop]);
+            int c2 = (S_BOX[temp] < 0 ? 256 + S_BOX[temp] : S_BOX[temp]);
 
             if (c1 > c2) {
                 swap(loop, temp);
@@ -141,11 +139,11 @@ public final class Spritz {
         whip();
         crush();
         whip();
-        this.a_register = 0;
+        a_register = 0;
     }
 
     private void squeeze(byte[] array) {
-        if (this.a_register > 0) {
+        if (a_register > 0) {
             shuffle();
         }
 
@@ -155,12 +153,12 @@ public final class Spritz {
     }
 
     private void absorbNibble(int val1) {
-        if (this.a_register == N / 2) {
+        if (a_register == N / 2) {
             shuffle();
         }
 
-        swap(this.a_register, (N / 2 + val1) & 0xFF);
-        this.a_register = (short) ((this.a_register + 1) & 0xFF);
+        swap(a_register, (N / 2 + val1) & 0xFF);
+        a_register = (short) ((a_register + 1) & 0xFF);
     }
 
     private void absorbByte(int val1) {
@@ -175,33 +173,33 @@ public final class Spritz {
     }
 
     public void absorbStop() {
-        if (this.a_register == N / 2) {
+        if (a_register == N / 2) {
             shuffle();
         }
 
-        this.a_register = (short) ((this.a_register + 1) & 0xFF);
+        a_register = (short) ((a_register + 1) & 0xFF);
     }
 
     public int drip() {
-        if (this.a_register > 0) {
+        if (a_register > 0) {
             shuffle();
         }
 
         update();
 
         // simulate unsigned
-        int temp1 = (this.z_register + this.k_register) & 0xFF;
+        int temp1 = (z_register + k_register) & 0xFF;
 
-        int temp2 = this.S_BOX[temp1];
-        temp2 = (this.i_register + (temp2 < 0 ? 256 + temp2 : temp2)) & 0xFF;
+        int temp2 = S_BOX[temp1];
+        temp2 = (i_register + (temp2 < 0 ? 256 + temp2 : temp2)) & 0xFF;
 
-        int temp3 = this.S_BOX[temp2];
-        temp3 = (this.j_register + (temp3 < 0 ? 256 + temp3 : temp3)) & 0xFF;
+        int temp3 = S_BOX[temp2];
+        temp3 = (j_register + (temp3 < 0 ? 256 + temp3 : temp3)) & 0xFF;
 
-        int temp4 = this.S_BOX[temp3];
+        int temp4 = S_BOX[temp3];
         temp4 = temp4 < 0 ? 256 + temp4 : temp4;
 
-        this.z_register = (short) temp4;
+        z_register = (short) temp4;
 
         return temp4;
     }
